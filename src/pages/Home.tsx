f3ebb62b04d664616e9fcbb2d0355dc3fd8ad15a
Harsh Rohila <rohilaharsh@gmail.com>
2020-09-30 20:44:46 +0530

    added google sign in styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,8 +26,16 @@ function Home() {
 		</div>
 		<div className="h-full">
 			<p className="">Coming soon...</p>  
-      <button onClick={onClick}>Sign in with Google</button>
       <Tile />
+      <div className="col s12 m6 offset-m3 center-align">
+          <a className="oauth-container btn darken-4 white black-text" onClick={onClick} style={{textTransform:'none'}}>
+              <div className="left">
+                  <img width="20px" style={{marginTop:'7px', marginRight:'8px'}} alt="Google sign-in" 
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+              </div>
+              Login with Google
+          </a>
+      </div>
 		</div>
     </div>
   )
